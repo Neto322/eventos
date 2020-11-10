5e3241a374b54c325c995a4f56d20a20b9d105a8
Neto322 <42181276+Neto322@users.noreply.github.com>
2020-11-09 19:21:55 -0600

    a
    a

---
+++ eventos/eventos/Models/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  eventos
+//
+//  Created by Alumno on 09/11/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import Foundation
---
+++ eventos/eventos/ViewControllers/CeldaEventoController.swift
@@ -0,0 +1,24 @@
+//
+//  CeldaEventoController.swift
+//  eventos
+//
+//  Created by Alumno on 09/11/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaEventoController : UITableViewCell
+{
+    
+    @IBOutlet weak var imgEvento: UIImageView!
+    @IBOutlet weak var lblTitulo: UILabel!
+    
+    @IBOutlet weak var lblLugar: UILabel!
+    
+    @IBOutlet weak var lblFecha: UILabel!
+    
+    
+    @IBOutlet weak var lblHora: UILabel!
+}
